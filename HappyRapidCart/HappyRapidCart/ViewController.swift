8a11a05ec0cca6c2b7518c4beea4f976ff5d8491
curiousjazz77 <10506008+curiousjazz77@users.noreply.github.com>
2020-11-15 20:58:02 -0600

    Add app skeleton

---
+++ HappyRapidCart/HappyRapidCart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyRapidCart
+//
+//  Created by Jasmine Omeke on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
